d71dbf4d5be8a9b041778a3c1eddc5b89dd3bb29
Praveen Davi <72861598+Praveendavi@users.noreply.github.com>
2020-10-17 11:42:57 +0530

    Updates

---
+++ precache-manifest.e0215b92059c448607d1845080ba4f3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31eb33cc109db82e4df3baacbef0e3d3",
+    "url": "/Praveendavi/React-Crud/index.html"
+  },
+  {
+    "revision": "cc47d78c50ac7e01cd11",
+    "url": "/Praveendavi/React-Crud/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "05ab0b90b547cefe7046",
+    "url": "/Praveendavi/React-Crud/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "cc47d78c50ac7e01cd11",
+    "url": "/Praveendavi/React-Crud/static/js/2.6e3e1c0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Praveendavi/React-Crud/static/js/2.6e3e1c0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05ab0b90b547cefe7046",
+    "url": "/Praveendavi/React-Crud/static/js/main.84665fd4.chunk.js"
+  },
+  {
+    "revision": "86eeb11ecc9182e29dcb",
+    "url": "/Praveendavi/React-Crud/static/js/runtime-main.b7bd4358.js"
+  }
+]);
